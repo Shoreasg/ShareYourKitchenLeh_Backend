e688d00501d401510319775780b653d2b568e069
GanWeiLun <garyganweilun@gmail.com>
2021-12-17 16:18:58 +0800

    setup local login

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 const express = require('express');
+const passport = require('passport')
+const session = require('express-session')
+const LocalStrategy = require('passport-local').Strategy
+const User = require('./models/user')
 const app = express();
 app.use(express.json())
+
+
+app.use(session({
+    secret: process.env.secret,
+    resave:false,
+    saveUninitialized: false
+}));
+
+
+// Configure passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// use static authenticate method of model in LocalStrategy
+passport.use(new LocalStrategy(User.authenticate()));
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 
 
