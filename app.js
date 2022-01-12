32ff0e3fc8bc28b479c63078764c7317a7d27ebb
Wei Lun <garyganweilun@gmail.com>
2022-01-13 01:02:46 +0800

    Changes for prod

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ app.use(express.json())
 app.use(cors({ origin: "http://localhost:3000", methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD', 'DELETE', 'PATCH'], credentials: true }));
 const userController = require('./controllers/localLoginController')
 const socialController = require('./controllers/socialLoginController')
-// app.enable('trust proxy')
+app.enable('trust proxy')
 
 app.use(session({
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false,
-    // cookie: {sameSite: "none", secure: true}
+    cookie: {sameSite: "none", secure: true}
 }));
 
 
